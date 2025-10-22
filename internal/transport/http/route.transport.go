910efd49f38b353806373dfb06a12a0fe93fa0a8
@raditya <radityaaydin@gmail.com>
2025-10-22 20:32:44 +0700

    fix: remove stray code from InitRoute function

--- internal/transport/http/route.transport.go
+++ internal/transport/http/route.transport.go
@@ -7,7 +7,6 @@ import (
 
 func (t *Transport) InitRoute() {
 	t.router.Use(t.middlewareController.CustomPanicHandler(), t.middlewareController.LoggingMiddleware())
-	bu
 	// Health check endpoint
 	t.router.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
